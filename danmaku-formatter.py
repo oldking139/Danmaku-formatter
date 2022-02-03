77c4c3d1978a40e0761dde929eded18a941dd97a
oldking139 <45203648+oldking139@users.noreply.github.com>
2022-02-03 19:59:23 +0800

    prod v1.0.2

--- danmaku-formatter.py
+++ danmaku-formatter.py
@@ -30,11 +30,5 @@ with open(input_file_path,'r',encoding='utf-8') as r:
 with open(input_file_path, 'w', encoding='utf-8') as w:
     for contents in lines:
         contents = contents.replace('  <','<')
-        w.write(contents)
-#多行合一
-with open(input_file_path,'r',encoding='utf-8') as r:
-    lines=r.readlines()
-with open(input_file_path, 'w', encoding='utf-8') as w:
-    for contents in lines:
-        contents = contents.replace('\n','')
+        contents = contents.replace('\n', '')
         w.write(contents)
