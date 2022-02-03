6260c0772d958e232d809c2bb992781ff1db6564
oldking139 <45203648+oldking139@users.noreply.github.com>
2022-02-03 19:54:53 +0800

    prod v1

--- danmaku-formatter.py
+++ danmaku-formatter.py
@@ -1,3 +1,4 @@
+import os
 input_file_path = str(input())
 print(input_file_path)
 
@@ -24,35 +25,16 @@ with open(input_file_path, 'w', encoding='utf-8') as w:
           w.write(contents)
 
 #去除行首空格
-def delspace(input_file_path):
-    txt=open(input_file_path,'r')
-    newtxt=open(input_file_path,'w')
-    lines=txt.readlines()
-    for line in lines:
-        newline=line[2:]
-        newtxt.write(newline)
-    txt.close()
-    newtxt.close()
-
-
-#多行合一
-with open(input_file_path,'r',encoding='utf-8') as r:
-    lines=r.readlines()
-with open(input_file_path, 'w', encoding='utf-8') as w:
-    a = ''  # 空字符（中间不加空格）
-    for line in lines:
-        a += line.strip('\n')  # strip()是去掉每行末尾的换行符\n 1
-    c = a.split()  # 将a分割成每个字符串 2
-    b = ' '.join(c)  # 将c的每个字符不以任何符号直接连接 3
-    # print(a)
-    # print(b)
-    for line in lines:
-         w.write(b)
-
-#去除每个element之间的空格
 with open(input_file_path,'r',encoding='utf-8') as r:
     lines=r.readlines()
 with open(input_file_path, 'w', encoding='utf-8') as w:
     for contents in lines:
-        contents = contents.replace('> <','><')
+        contents = contents.replace('  <','<')
+        w.write(contents)
+
+with open(input_file_path,'r',encoding='utf-8') as r:
+    lines=r.readlines()
+with open(input_file_path, 'w', encoding='utf-8') as w:
+    for contents in lines:
+        contents = contents.replace('\n','')
         w.write(contents)
